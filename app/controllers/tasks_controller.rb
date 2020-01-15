f0659c7cd6cd0c8330b4dea6d97dcc7a7b928d23
ASO1801033 <1801033@s.asojuku.ac.jp>
2020-01-15 13:28:03 +0900

    third commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,31 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.all #登録済みの全タスクを取得
   end
 
   def new
-    @task = Task.new
+    @task = Task.new #入力フォームを表示するため@task = Task.newでTaskのモデルオブジェクトを作成
   end
 
   def create
-    @task = Task.create(task_params)
-    redirect_to tasks_path
+    @task = Task.create(task_params) #POSTで送信されたパラメータをデータベースに登録
+    redirect_to tasks_path #登録後タスク一覧画面に遷移
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = Task.find(params[:id]) #Tasksテーブルのidを元に検索
   end
 
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to tasks_url
+    redirect_to tasks_url #更新後タスク一覧画面に遷移
   end
 
-  def destroy
+  def destroy #削除機能(delete)
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_url
+    redirect_to tasks_url #削除後タスク一覧画面に遷移
   end
 
   private
